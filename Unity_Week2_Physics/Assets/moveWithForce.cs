1116db1b2f9eb48f08f1aefbf4a2cc32695da29b
Rohil Aniruth <anirr579@newschool.edu>
2016-09-08 19:56:27 -0400

    Physics based game, early stages. Learning trap doors. Learning code on how to move object around (bouncing ball.)

---
+++ Unity_Week2_Physics/Assets/moveWithForce.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveWithForce : MonoBehaviour {
+
+    // calling certain elements we're going to need, a call to adjust speed and a call to identify the rigidbody
+    float acceleration = 10;
+    Rigidbody thisRB;
+
+	// Use this for initialization
+	void Start () {
+
+        // loads up the rigidbody and gets it ready for use in the update section
+        thisRB = GetComponent<Rigidbody>();
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // this line gets your object to move right and left, and understand the acceleration/speed you want
+        // thisRB is the rigidbody on the object and AddForce is adding the force/movement
+       thisRB.AddForce( Vector3.right * Input.GetAxis( "Horizontal" ) * acceleration);
+        // this line does the same as above, but instead of moving right or left, this line moves the object up and down
+       thisRB.AddForce( Vector3.up * Input.GetAxis( "Vertical" ) * acceleration );
+    }
+}
---
+++ Unity_Week2_Physics/Assets/trapDoorSwitch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trapDoorSwitch : MonoBehaviour
+{
+
+    float rotationSpeed = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if i hit a specific key, open the trap door
+        // Input.GetKey ... is how you get keys. 
+        if (Input.GetKey(KeyCode.T))
+        {
+            // if key 't' is hit
+            print("open trapdoor");
+
+            // rotate to -90 degrees
+            // by rotation speed
+
+            transform.Rotate(Vector3.right * rotationSpeed);
+        }
+    }
+}
+
